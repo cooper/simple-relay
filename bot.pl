30fdd2e8ea782fe168fd1a37e54c59c828a9838a
Mitchell Cooper <cooper879@msn.com>
2012-11-23 21:50:25 -0500

    simple-relay now supports over nine thousand connections.

--- bot.pl
+++ bot.pl
@@ -19,71 +19,61 @@ say 'hi!';
 ### OPTIONS ###
 ###############
 
-# first server
-my %opts1 = (
-    name => 'valleynode',
-    nick => 'Sharon',
-    user => 'sharon',
-    real => 'Sharon Herget',
-    host => 'irc.valleynode.net',
-    port => 6667,
-    chan => ['#1lobby']
+my %servers = (
+    valleynode => {
+        nick => 'Sharon',
+        user => 'sharon',
+        real => 'Sharon Herget',
+        host => 'irc.valleynode.net',
+        port => 6667,
+        chan => ['#1lobby']
+    },
+    alphafaggot => {
+        nick => 'Sharon',
+        user => 'sharon',
+        real => 'Sharon Herget',
+        host => 'irc.alphachat.net',
+        port => 6667,
+        chan => ['#1lobby', '#cooper']
+    }
 );
 
-# second server
-my %opts2 = (
-    name => 'alphafaggot',
-    nick => 'Sharon',
-    user => 'sharon',
-    real => 'Sharon Herget',
-    host => 'irc.alphachat.net',
-    port => 6667,
-    chan => ['#1lobby', '#cooper']
-);
+###################
+### END OPTIONS ###
+###################
 
-# setup IO::Async
+my @ircs;
+
+# set up IO::Async
 my $loop = IO::Async::Loop->new();
 
-my ($name1, $name2) = (
-    delete $opts1{name},
-    delete $opts2{name}
-);
-
-my @chan1 = @{$opts1{chan}};
-my @chan2 = @{$opts2{chan}};
-delete $opts1{chan}; delete $opts2{chan};
-
-# create IRC objects
-my $irc1 = IRC::Async->new(%opts1);
-my $irc2 = IRC::Async->new(%opts2);
-
-$irc1->{server_name} = $name1;
-$irc2->{server_name} = $name2;
-
-$irc1->{autojoin} = \@chan1;
-$irc2->{autojoin} = \@chan2;
-
-$loop->add($irc1);
-$loop->add($irc2);
-
-$irc1->connect;
-$irc2->connect;
-
-#$irc1->attach_event(raw => sub { say "@_" });
-
-$irc1->attach_event(privmsg => sub {
-    my ($irc, $who, $chan, $what) = @_;
-    return unless defined $chan->{name};
-    $irc1->send("PRIVMSG $_ :\2<$$who{nick} (\2$$chan{name}\2)>\2 $what") foreach grep { lc $_ ne lc $chan->{name} } @chan1;
-    $irc2->send("PRIVMSG $_ :\2<$$who{nick} (\2$name1\2)>\2 $what") foreach @chan2;
-});
-
-$irc2->attach_event(privmsg => sub {
-    my ($irc, $who, $chan, $what) = @_;
-    return unless defined $chan->{name};
-    $irc2->send("PRIVMSG $_ :\2<$$who{nick} (\2$$chan{name})>\2 $what") foreach grep { lc $_ ne lc $chan->{name} } @chan2;
-    $irc1->send("PRIVMSG $_ :\2<$$who{nick} (\2$name2\2)>\2 $what") foreach @chan1;
-});
+# set up servers.
+foreach my $name (keys %servers) {
+    my $opts  = $servers{$name};
+    my $chans = delete $opts->{chan};
+    my $irc   = IRC::Async->new(%$opts);
+    push @ircs, $irc;
+    $irc->{server_name} = $name;
+    $irc->{autojoin}    = $chans;
+    $loop->add($irc);
+    $irc->connect;
+    
+    $irc->attach_event(privmsg => sub {
+        my (undef, $who, $chan, $what) = @_;
+        return unless defined $chan->{name};
+        
+        # send to all of the other channels on this server.
+        $irc->send("PRIVMSG $_ :\2<$$who{nick} (\2$$chan{name}\2)>\2 $what") foreach grep { lc $_ ne lc $chan->{name} } @$chans;
+        
+        # send to other networks.
+        foreach my $oirc (@ircs) {
+            next if $oirc == $irc;
+            $oirc->send("PRIVMSG $_ :\2<$$who{nick} (\2$name\2)>\2 $what") foreach @{$oirc->{autojoin}};
+        }
+        
+    });
+    
+}
 
 $loop->loop_forever;
 
